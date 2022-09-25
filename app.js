8d588e8f24d1420de4fbb76d7674997d20e2d06d
Michael Barros <michaelbenamimsb@hotmail.com>
2022-09-25 15:41:55 -0300

    add heroku config

--- app.js
+++ app.js
@@ -10,87 +10,87 @@ app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
 // ###############################################
-let staticData = [{
-        id: "1",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "2",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "3",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "4",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "5",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "6",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "7",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "8",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "9",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-    {
-        id: "10",
-        attributeA: "AAA AAAAA",
-        attributeB: "BBBB BBBB",
-        attributeC: 83.18,
-        attributeD: 53655078,
-        attributeE: true
-    },
-];
+//let staticData = [{
+//        id: "1",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "2",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "3",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "4",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "5",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "6",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "7",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "8",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "9",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//    {
+//        id: "10",
+//        attributeA: "AAA AAAAA",
+//        attributeB: "BBBB BBBB",
+//        attributeC: 83.18,
+//        attributeD: 53655078,
+//        attributeE: true
+//    },
+//];
 
 let message;
 
