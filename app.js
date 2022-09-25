badece595c690c884a4f58badcd2aae0866b4e6b
Michael Barros <michaelbenamimsb@hotmail.com>
2022-09-25 15:10:25 -0300

    add heroku config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const res = require('express/lib/response');
 const manipulacaoDeMsg = require('../avaliacao-web-service/mensagens');
-const staticData = require('../avaliacao-web-service/mensagens')
+const staticData = require('../avaliacao-web-service/staticData')
 
 const app = express();
 
