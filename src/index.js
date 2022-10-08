2658a2b6062c22332a5c8cdcbddcd0e2755328a9
Michael Barros <michaelbenamimsb@hotmail.com>
2022-10-08 20:23:46 -0300

    refactor: attribute checker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { express } = require('express');
+const express = require('express');
 const res = require('express/lib/response');
 const getMessage = require('./messagestorage');
 const staticData = require('./data');
