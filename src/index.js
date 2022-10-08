0d605f31a1080c0aa760508bae678d887d5babd9
Michael Barros <michaelbenamimsb@hotmail.com>
2022-10-08 18:12:28 -0300

    add: post method

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ function attributeChecker(req) {
         errors["attributeD"] = (manipulacaoDeMsg('positiveNumber', 'attributeD'))
     }
 
-    if (req.body.attributeE == "") {
+    if (req.body.attributeE === "") {
         res.send(req.body.attributeE)
         errors["attributeE"] = (manipulacaoDeMsg('requiredField', 'attributeE'))
     } else if (typeof req.body.attributeE != "boolean") {
