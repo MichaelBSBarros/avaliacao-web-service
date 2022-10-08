7b8f3c84c76f1ca876dfd616f3edc045cfc274bc
Michael Barros <michaelbenamimsb@hotmail.com>
2022-10-08 19:30:47 -0300

    add: del method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,53 @@ const express = require('express');
 const res = require('express/lib/response');
 const getMessage = require('./messagestorage');
 const staticData = require('./data')
-const attributeChecker = require('./attributechecker')
+    //const attributeChecker = require('./attributechecker')
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+function attributeChecker(req) {
+    let errors = {}
+
+    if (!req.body.attributeA || req.body.attributeA === null) {
+        errors["attributeA"] = (getMessage('requiredField', 'attributeA'))
+    } else if (!/^[a-zA-Z]+$/.test(req.body.attributeA)) {
+        errors["attributeA"] = (getMessage('onlyLetters', 'attributeA'))
+    } else if (req.body.attributeA.length <= 1) {
+        errors["attributeA"] = (getMessage('lengthMoreThan', 'attributeA', 2))
+    } else if (req.body.attributeA.length > 100) {
+        errors["attributeA"] = (getMessage('lengthLessThan', 'attributeA', null, 100))
+    }
+
+    if (!req.body.attributeB || req.body.attributeB === null) {
+        errors["attributeB"] = (getMessage('requiredField', 'attributeB'))
+    } else if (req.body.attributeB.length <= 8) {
+        errors["attributeB"] = (getMessage('lengthMoreThan', 'attributeB', 8))
+    } else if (req.body.attributeB.length > 40) {
+        errors["attributeB"] = (getMessage('lengthLessThan', 'attributeB', null, 40))
+    }
+
+    if (!req.body.attributeC || req.body.attributeC === null) {
+        errors["attributeC"] = (getMessage('requiredField', 'attributeC'))
+    } else if (isNaN(req.body.attributeC)) {
+        errors["attributeC"] = (getMessage('isNumber', 'attributeC'))
+    }
+
+    if (!req.body.attributeD || req.body.attributeD === null) {
+        errors["attributeD"] = (getMessage('requiredField', 'attributeD'))
+    } else if (isNaN(req.body.attributeD) || !Number.isInteger(req.body.attributeD)) {
+        errors["attributeD"] = (getMessage('isInt', 'attributeD'))
+    } else if (req.body.attributeD < 0) {
+        errors["attributeD"] = (getMessage('positiveNumber', 'attributeD'))
+    }
+
+    if (req.body.attributeE === "" || req.body.attributeE === null) {
+        errors["attributeE"] = (getMessage('requiredField', 'attributeE'))
+    } else if (typeof req.body.attributeE != "boolean") {
+        errors["attributeE"] = (getMessage('isBoolean', 'attributeE'))
+    }
+    return errors;
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
