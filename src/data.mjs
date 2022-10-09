bb7b76d6202f4b5dfb09229d056f906bdc540756
Michael Barros <michaelbenamimsb@hotmail.com>
2022-10-09 14:00:31 -0300

    refactor: data test

--- src/data.mjs
+++ src/data.mjs
@@ -1,82 +1,34 @@
 let staticData = [{
         "_id": "e190e822-b23c-4119-ab44-0ca983ea427b",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
+        "nome": "Isaac Bento Lopes",
+        "email": "isaac.b.lopes@saude.pe.gov.br",
+        "senha": "GbRFDNcqAz",
+        "telefone_fixo": "8131840001",
         "usuario_ouvidor": true
     },
     {
         "_id": "421cc2d5-5ac1-4f13-b21f-6f9dcc1df6f4",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
+        "nome": "Mauricéia Tavares da Silva",
+        "email": "mauriceia_silva_1967@zulix.com",
+        "senha": "ILhSaTyZP1",
+        "telefone_fixo": "8132327212",
+        "usuario_ouvidor": false
     },
     {
         "_id": "28845e73-0d2a-46e6-a7f8-747961613a5d",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
+        "nome": "Renata Aurora de Rodrigues e Silva",
+        "email": "renata.a.rodrigues@saude.pe.gov.br",
+        "senha": "uxx4nrSe1E",
+        "telefone_fixo": "8131840001",
         "usuario_ouvidor": true
     },
     {
         "_id": "7059e888-5e19-4399-b4d5-0c410b809115",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
+        "nome": "Marcos de Almeida Palhares",
+        "email": "marcos.almeida@eletrovip.com",
         "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
-    },
-    {
-        "_id": "2e792aec-c197-4a4b-bfe3-b2313a8e3dcd",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
-    },
-    {
-        "_id": "f79f61c9-38a4-485f-b4cc-89c195a4fd86",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
-    },
-    {
-        "_id": "fdff2daa-bc7d-4afb-9dcb-2a23237e8ffc",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
-    },
-    {
-        "_id": "1d96b51c-0095-4878-bd41-31ee5f20a725",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
-    },
-    {
-        "_id": "835a7af2-0a1e-40e8-ba67-97f18a97e04e",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
-    },
-    {
-        "_id": "4c65da2b-3225-40fb-9a9d-3faf1b3b107e",
-        "nome": "AAA AAAAA",
-        "email": "BBBB BBBB",
-        "senha": 83.18,
-        "telefone_fixo": 53655078,
-        "usuario_ouvidor": true
+        "telefone_fixo": "8132256752",
+        "usuario_ouvidor": false
     }
 ];
 export default staticData;
